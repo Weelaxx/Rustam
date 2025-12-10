59a94c08acc4babf7ec666dc7bb315b8f09e6cfb
Weelaxx <jasd92310@gmail.com>
2025-12-10 21:19:40 +0300

    FIX qweqweqwe

--- tabasaran.kt
+++ tabasaran.kt
@@ -2,7 +2,7 @@ fun main() {
     val secretNumber = (1..100).random()
     var attempts = 0
     
-    println("Угадай число от 1 до 100!")
+
     
     while (true) {
         val guess = readlnOrNull()?.toIntOrNull()
